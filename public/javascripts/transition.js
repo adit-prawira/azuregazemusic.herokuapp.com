58e973ad4f0b9112731f25208b1e32bcbe917e80
Aditya Prawira <adityaprawira96@gmail.com>
2020-12-06 02:09:50 +1100

    Add browser tab icon and fix page flexibility and interactive functionalities

--- public/javascripts/transition.js
+++ public/javascripts/transition.js
@@ -17,31 +17,31 @@ function autoChange() {
 
     switch (slideIndex) {
         case 0:
-            container.style.backgroundColor = "rgb(139,212,193)";
+            container.style.backgroundColor = "#72D7C0";
             break;
         case 1:
-            container.style.backgroundColor = "rgb(255,254,254)";
+            container.style.backgroundColor = "#FFFEFE";
             break;
         case 2:
-            container.style.backgroundColor = "rgb(99,137,141)";
+            container.style.backgroundColor = "#588A8E";
             break;
         case 3:
-            container.style.backgroundColor = "rgb(215,194,172)";
+            container.style.backgroundColor = "#DBC2A8";
             break;
         case 4:
-            container.style.backgroundColor = "rgb(99,147,122)";
+            container.style.backgroundColor = "#549578";
             break;
         case 5:
-            container.style.backgroundColor = "rgb(68,97,177)";
+            container.style.backgroundColor = "#3C62B7";
             break;
         case 6:
-            container.style.backgroundColor = "rgb(198,212,165)";
+            container.style.backgroundColor = "#C3D59F";
             break;
         case 7:
-            container.style.backgroundColor = "rgb(159,183,240)";
+            container.style.backgroundColor = "#99B8F5";
             break;
         case 8:
-            container.style.backgroundColor = "rgb(11,15,40)";
+            container.style.backgroundColor = "#081029";
             break;
         default:
             container.style.backgroundColor = "#fffefe";
