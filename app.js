8cc25218319f7e05ba4c02d2eee337f0d20ec001
Aditya Prawira <adityaprawira96@gmail.com>
2020-12-17 19:25:17 +1100

    Fix some errors

--- app.js
+++ app.js
@@ -157,6 +157,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log("Run Server:");
 });
